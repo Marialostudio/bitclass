54c265193e1b9c4d99026a755d5c5e533c5f9152
Marialostudio <89900388+Marialostudio@users.noreply.github.com>
2024-04-18 23:36:43 -0500

    ejercicio Vanilla

--- javascript/formulario/js/main.js
+++ javascript/formulario/js/main.js
@@ -1,4 +1,29 @@
 'use strict'
-/*IMPORTS*/
-/*VARIABLES*/
+/* IMPORTS */
+/* VARIABLES */
 const d = document;
+
+const $signUp = d.getElementById('signUp');
+
+/* EVENTS */
+d.addEventListener('DOMContentLoaded', () => {
+    eventListeners()
+});
+
+/* FUNCTIONS */
+function eventListeners() {
+    $signUp.addEventListener('submit', handleSubmit);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const credentials = {
+        id: 'fakeID1',
+        name: $signUp.name.value,
+        email: $signUp.email.value,
+        password: $signUp.password.value,
+    };
+    console.log(credentials)
+}
+/* CODE */
